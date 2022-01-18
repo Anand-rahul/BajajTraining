d2ddc06629ebabdaeed8a272c1f37601b8544d85
Rahul Anand <anandrahul596@gmail.com>
2022-01-18 15:08:30 +0530

    your message

---
+++ firs.java
@@ -0,0 +1,138 @@
+package fristt;
+
+public class firs {
+	/*static int a=10;	//static variable
+	void method() {
+		int b=20;		//local variable
+	}*/
+	/*public static void main(String[] args) {
+		System.out.println("FrostBite !!");
+		int c=30;		//instance variable
+		int a=10,b=20;
+		c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		int c=30;		//instance variable
+		float a=c;	//Widening
+		System.out.println(c+"+"+a);
+	}
+	public static void main(String[] args) {
+		int a=20;
+		int b=3;
+		float c=a/b;
+		float d=20.666f;
+		int x=(int)d;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(x);
+	}
+	/*public static void main(String[] args) {
+		int a=10;
+		
+		System.out.println(a++);//10
+		System.out.println(++a);//12
+		System.out.println(a--);//12
+		System.out.println(--a);//10
+	}
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		String f="Rahul";
+		String l=" Anand";
+		String full=f+l;
+		System.out.println(full);
+	}
+	
+	public static void main(String[] args) {
+		String f="Rahul";
+		String l=" Anand";
+		String full=f+l;
+		System.out.println(full.toUpperCase());
+		String ms="We are in first \"Java prog";//odd no's of quotes
+		System.out.println(ms);
+		System.out.println(Math.max(40, 45));
+		System.out.println(Math.ceil(40.0/23));
+		System.out.println(Math.floor(40.0/23));
+		System.out.println(Math.random());
+		boolean a =true;
+		boolean b=false;
+		System.out.println(a||b);
+		
+		if(10>5) {
+			System.out.println("Lg");
+		}
+		else {
+			System.out.println("Lg1");
+		}
+		
+		int b1=20;
+		String res =(b1<40)?"If ran":	"else ran";
+		System.out.println(res);
+		
+		
+		
+		
+		
+		int a=10,b=20,c=30;
+		if(a>b && a>c) {
+				System.out.println(a);
+			
+		}
+		else if(b>a && b>c) {
+				System.out.println(b);
+			
+		}
+		else if(c>b && c>a) {
+				System.out.println(c);
+		}
+		int f=(a>b)?a:b;
+		int large=(c>f)?c:f;
+		System.out.println(large);
+	}*/
+	public static void main(String[] args) {
+		
+		int x=4;
+		switch(x)
+		{
+		case 0:
+			System.out.println("DAy is Sunday");
+			break;
+		case 1:
+			System.out.println("DAy is Monday");
+			break;
+		case 2:
+			System.out.println("DAy is Tuesday");
+			break;
+		case 3:
+			System.out.println("DAy is wednesday");
+			break;
+		case 4:
+			System.out.println("DAy is thursday");
+			break;
+		case 5:
+			System.out.println("DAy is friday");
+			break;
+		case 6:
+			System.out.println("DAy is Saturday");
+			break;
+		default:
+			System.out.println("dayzzzzzzz");
+		}
+		
+		
+	
+}
+	}
+//single line
+/*
+	multiple line
+*/
